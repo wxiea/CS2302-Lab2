649c887903d12c56526139e6148ab56aa65e93af
wxiea <43360560+wxiea@users.noreply.github.com>
2019-02-22 21:35:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import random
+
+def Random(small,large,length):
+    randomlist = []
+    for i in range(length):
+        randomlist.append(random.randint(small,large))
+    return randomlist
+
 def BubbleSort(list):
     for i in range(len(list)-1):
         for j in range(len(list)-i-1):
             if list[j] > list[j+1]:
                 list[j], list[j+1] = list[j+1], list[j]
-    return FindMiddle(list)
+    return list,FindMiddle(list)
 
 def Merge(left, right):
     result = []
@@ -23,7 +31,7 @@ def MergeSort(list):
     right = MergeSort(list[mid:])
     return Merge(left,right)
 
-def QuickSort(list, left, right):
+def QuickSort(list,left,right):
     i = left 
     j = right
     if i >= j:
@@ -39,7 +47,7 @@ def QuickSort(list, left, right):
     list[i] = key 
     QuickSort(list, left, i-1)
     QuickSort(list, j+1, right)
-    return FindMiddle(list)
+    return list,FindMiddle(list)
 
 def FindMiddle(list):
     M = len(list)//2
@@ -52,13 +60,23 @@ def Median(list):
     M = len(list) // 2
     return (list[M] + list[~M]) / 2
 
+"""
+def Median(L): 
+    C = Copy(L)
+    Sort(C) 
+    return ElementAt(C,GetLength(C)//2)
+"""
+
 a = [1,5,4,7,8,3,6,11,21]
 b = [9,5,3,0,4,6,5,7]
 c = [10,60,54,87,52,1,2,36,54,21]
 
-print(BubbleSort(c))
-#Toprint = MergeSort(c)
-#print(MergeSort(c))
-#print(QuickSort(b,0,7))
-print(Median(c))
+Listone = Random(0,10, 9)
+print(Listone)
+
+print(BubbleSort(Listone))
+#Toprint = MergeSort(Listone)
+#print(MergeSort(Listone))
+#print(QuickSort(Listone,0,8))
+print(Median(Listone))
 #print(FindMiddle(Toprint))
